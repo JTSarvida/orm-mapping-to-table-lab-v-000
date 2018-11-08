4016c740dcc4e3ae6f3313c4965b21f9340bdbdf
Joshua Tan <jtsarvida@gmail.com>
2018-11-08 10:20:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,6 +20,9 @@ class Student
       DB[:conn].execute(sql)
   end
   
+  def self.save
+    sql = <<-SQL
+      
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
